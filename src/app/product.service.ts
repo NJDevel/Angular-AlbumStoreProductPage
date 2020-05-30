36e35e26d8c55cd082961b075cf6ace659e04b82
Abraham Leal <aleal629@gmail.com>
2020-05-30 08:38:54 -0400

    Service Fixes

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,6 @@
 import { Http, Response } from '@angular/http'
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs/add/operator/map';
+import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ProductService {
@@ -10,11 +10,9 @@ export class ProductService {
     private _albumUrl : string = '../assets/album.json';
 
 
-    // getAlbum(id: number) {
-    //   return this._http.get(this._albumUrl)
-    //     .pipe(
-    //       map((response: Response) => response.json)
-    //     )
+    getAlbum(id: number) {
+      return this._http.get(this._albumUrl)
+        .map((response) => response.json());
     }
 
 
